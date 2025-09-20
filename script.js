38abaf138a1acddb33024d7678b9fa04c40e9ae9
Kazeem Bolaji <kazeem.s.bolaji@gmail.com>
2025-09-20 03:09:37 +0100

    Worked on the media query for responsiveness

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+const menuBtn = document.querySelector(".open-menu i")
+const closeBtn = document.querySelector(".close-menu i")
+const menu = document.querySelector(".links")
 
+menuBtn.addEventListener("click", ()=>{
+    menu.classList.add("show")
+    closeBtn.classList.add("active")
+    menuBtn.style.display = "none"
+})
+
+closeBtn.addEventListener("click", ()=>{
+    menu.classList.remove("show")
+    closeBtn.classList.remove("active")
+    menuBtn.style.display = "block"
+})
 
 
 var swiper = new Swiper(".mySwiper", {
@@ -17,4 +31,21 @@ var swiper = new Swiper(".mySwiper", {
             delay: 3000, // Autoplay with 3 seconds delay
             disableOnInteraction: false, // Continue autoplay after user interaction
         },
+        breakpoints: {
+            // when window width is >= 0px
+            0: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            // when window width is >= 580px
+            580: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            }
+        }
 });
+
+
+
+
+
